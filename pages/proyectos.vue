516df0cbb9674ae0a5f8dc45110576af0e931e9b
sescobar9 <santiagoesco99@gmail.com>
2021-10-07 18:12:50 -0500

    ultimos cambios

--- pages/proyectos.vue
+++ pages/proyectos.vue
@@ -1,13 +1,1 @@
-<template>
-    <div>
-        <v-btn color="success">
-            Eliminar Proyectos
-        </v-btn>
-        <v-btn color="success">
-            Actualizar Proyectos
-        </v-btn>
-        <v-btn color="success">
-            Crear Proyectos
-        </v-btn>
-    </div>
-</template>
+
